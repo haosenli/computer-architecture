58be76204f903a03efedc2c445552fbf94cc8cd7
Haosen Li <haosenli@Haosens-MacBook-Pro-15.local>
2022-10-26 13:16:42 -0700

    Added Python files.

---
+++ py_files/alu_gen.py
@@ -0,0 +1,6 @@
+for i in range(16):
+    print(f'\tor #5 z{i}(temp_zero0[{i}], result[{i*4+0}], result[{i*4+1}], result[{i*4+2}], result[{i*4+3}]);')
+    
+for i in range(4):
+    print(f'\tor #5 z{i+16}(temp_zero1[{i}], temp_zero0[{i*4+0}], temp_zero0[{i*4+1}], temp_zero0[{i*4+2}], temp_zero0[{i*4+3}]);')
+    
