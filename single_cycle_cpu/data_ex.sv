935b27153554c5687a1c60e3fa111fa96088608a
Peter <pt1@uw.edu>
2022-11-20 15:22:13 -0800

    added testbench

--- single_cycle_cpu/data_ex.sv
+++ single_cycle_cpu/data_ex.sv
@@ -5,21 +5,20 @@
  * This file contains the datapath for execute in a CPU.
  *
  * Inputs:
- * ReadData1   	    - 64 bits, ReadData1 result from register signal.
- * ReadData2   	    - 64 bits, ReadData2 result from register signal.
+ * ReadData1   	- 64 bits, ReadData1 result from register signal.
+ * ReadData2   	- 64 bits, ReadData2 result from register signal.
  * PC			   	- 64 bits, Data memory write data.
- * ALU_imm_extend   - 64 bits, ALU_imm but 64 bit signal.
+ * ALU_imm_extend - 64 bits, ALU_imm but 64 bit signal.
  * BR_to_shift		- 64 bits, BR_addr but 64 bit signal.
- * ALUop			- 3 bits, Operation for ALU.
+ * ALUop				- 3 bits, Operation for ALU.
  * ALUsrc			- 1 bit, ALUsrc control signal.
- * UnCondBr			- 1 bit, UnCondBr control signal.
  * update			- 1 bit, Update output signals
  *
  * Outputs:
- * alu_result 		- 64 bits, ALU result.
- * ReadData2_out    - 64 bits, ReadData2 signal. 
+ * ALU_result 		- 64 bits, ALU result.
+ * ReadData2_out  - 64 bits, ReadData2 signal. 
  * new_PC2      	- 64 bits, new PC value.
- * negative     	- 1 bit, True if output is negative, false otherwise.
+ *negative     	- 1 bit, True if output is negative, false otherwise.
  * zero         	- 1 bit, True if output is zero, false otherwise.
  * overflow     	- 1 bit, True if output overflowed, false otherwise.
  * carry_out    	- 1 bit, Carry out signal.
@@ -28,8 +27,8 @@
 module data_ex(
 	input  logic [63:0] ReadData1, ReadData2, PC, ALU_imm_extend, BR_to_shift,
 	input  logic [2:0] ALUop,
-	input  logic ALUsrc, UnCondBr, update,
-	output logic [63:0] alu_result, ReadData2_out, new_PC2,
+	input  logic ALUsrc, update,
+	output logic [63:0] ALU_result, ReadData2_out, new_PC2,
 	output logic negative, zero, overflow, carry_out
 	);
 	
@@ -41,7 +40,7 @@ module data_ex(
 	mux64_2x1 add_2 (.sel(ALUsrc), .A(ALU_imm_extend), .B(ReadData2), .out(add2));
 	
 	// ALU to compute value
-	alu compute (.A(ReadData1), .B(add2), .cntrl(ALUop), .result(alu_result), .negative(temp_neg), 
+	alu compute (.A(ReadData1), .B(add2), .cntrl(ALUop), .result(ALU_result), .negative(temp_neg), 
 				.zero(temp_zero), .overflow(temp_overflow), .carry_out(temp_carry_out));
 	
 	// shifts BR_addr by 2
@@ -59,4 +58,47 @@ module data_ex(
 	// assign ReadData2 to output
 	assign ReadData2_out = ReadData2;
 	
+endmodule
+
+`timescale 10ps/1ps
+module data_ex_testbench();
+	logic [63:0] ReadData1, ReadData2, PC, ALU_imm_extend, BR_to_shift;
+	logic [2:0] ALUop;
+	logic ALUsrc, update;
+	logic [63:0] ALU_result, ReadData2_out, new_PC2;
+	logic negative, zero, overflow, carry_out;
+	
+	data_ex dut (.*);
+	
+	initial begin
+	// TEST 1: See if update off changes flags and check PC value
+	ReadData1 <= 64'b0000000000000000000000000000000000000000000000000000001010101010;
+	ReadData2 <= 64'b0000000000000000000000000000000000000000000000000000000101010101;
+	PC <= 64'b0000000000000000000000000000000000000000000000000000000000000000;
+	ALU_imm_extend <= 64'b0000000000000000000000000000000000000000000000000000000000000001;
+	BR_to_shift <= 64'b0000000000000000000000000000000000000000000000000000000000000010;
+	ALUop <= 3'b010;
+	ALUsrc <= 1'b0;
+	update <= 1'b0;	#1000;
+	
+	// TEST 2: See if update on changes flags and change ALU input to be ALU_imm
+	ReadData1 <= 64'b0000000000000000000000000000000000000000000000000000001010101010;
+	ReadData2 <= 64'b0000000000000000000000000000000000000000000000000000000101010101;
+	PC <= 64'b0000000000000000000000000000000000000000000000000000000000000000;
+	ALU_imm_extend <= 64'b0000000000000000000000000000000000000000000000000000000000000001;
+	BR_to_shift <= 64'b0000000000000000000000000000000000000000000000000000000010000000;
+	ALUop <= 3'b010;
+	ALUsrc <= 1'b1;
+	update <= 1'b1;	#1000;
+	
+	// TEST 3: Update off and see if flags change
+	ReadData1 <= 64'b1111111111111111111111111111111111111111111111111111101010101010;
+	ReadData2 <= 64'b0000000000000000000000000000000000000000000000000000000000010101;
+	PC <= 64'b0000000000000000000000000000000000000000000000000000000000000000;
+	ALU_imm_extend <= 64'b0000000000000000000000000000000000000000000000000000000000000001;
+	BR_to_shift <= 64'b0000000000000000000000000000000000000000000000000000000010000000;
+	ALUop <= 3'b010;
+	ALUsrc <= 1'b1;
+	update <= 1'b0;	#1000;
+	end
 endmodule
